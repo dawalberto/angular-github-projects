2876c996b5f3c56403176b6ee0f78006781a30a3
dawalberto <albertoinformatica97@gmail.com>
2022-06-21 18:37:04 +0200

    getting repositories works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RepositoriesService } from './repositories.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-github-projects';
+  repositories
+
+  constructor(private repoService: RepositoriesService) {}
+
+  ngOnInit(): void {
+    this.repositories = this.repoService.getRepositories()
+    console.log(this.repositories)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RepositoriePreviewComponent } from './repositorie-preview/repositorie-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoriePreviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
